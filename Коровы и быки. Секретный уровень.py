b90e4e3f42081135b12c6e7aa9097f984dd27b64
alex <106116143+maslov-cool@users.noreply.github.com>
2024-03-12 19:57:24 +0300

    Update Коровы и быки. Секретный уровень.py
    Исправлены баги

--- Коровы и быки. Секретный уровень.py
+++ Коровы и быки. Секретный уровень.py
@@ -24,14 +24,13 @@ def bulls_and_cows(list_human_num: list, list_bot_num: list) -> (int, int):
     for i in range(len(l2)):
         if l2[i] == l1[i]:
             bulls += 1
-            dict_['bulls'].append(i)
-            l1[i] = ''
-            l2[i] = ''
+            if i not in dict_['bulls']:
+                dict_['bulls'].append(i)
     for i in range(len(l2)):
-        if l2[i] and l2[i] in l1 and l2[i] != l1[i]:
-            cows += 1
-            dict_['cows'].append(l2[i])
-            l1[l1.index(l2[i])] = ''
+        if l2[i] in l1:
+            if l2[i] and l2[i] not in dict_['cows'] and i not in dict_['bulls']:
+                cows += 1
+                dict_['cows'].append(l2[i])
     return bulls, cows
 
 
@@ -39,8 +38,9 @@ def check(human_num_: int, bot_num_: int) -> bool:
     global dict_
     l1 = number_to_list(human_num_)
     l2 = number_to_list(bot_num_)
-    if all(l1[i] == l2[i] for i in dict_['bulls']) and all(j in [l1[i] for i in range(len(l1))
-                                                                 if i not in dict_['bulls']] for j in dict_['cows']):
+    # в процессе генерации мы генерируем только цифры, индексы которых не в dict_['bulls']
+    if len(str(bot_num_)) == 4 and all(j in [l2[i] for i in range(len(l2)) if i not in dict_['bulls']]
+                                       for j in dict_['cows']):
         return True
     return False
 
@@ -50,10 +50,11 @@ print("Здравствуйте, добро пожаловать в игру 'К
 bot_num = random.randint(1000, 9999)
 print('Компьютер загадал число, попробуйте его угадать')
 
-human_num = int(input())
+human_num = input()
 while not check_num(human_num):
     print('Некорректный ввод')
-    human_num = int(input())
+    human_num = input()
+human_num = int(human_num)
 
 bulls_cows = bulls_and_cows(number_to_list(human_num), number_to_list(bot_num))
 history.append((attempt, *bulls_cows))
@@ -63,8 +64,12 @@ print(f'Количество коров: {bulls_cows[1]}')
 while bot_num != human_num:
     bot_num = [i for i in str(human_num)]
     for i in range(len(bot_num)):
-        if i not in dict_['bulls']:
-            bot_num[i] = str(random.randint(0, 9))
+        if i:
+            if i not in dict_['bulls']:
+                bot_num[i] = str(random.randint(0, 9))
+        else:
+            if i not in dict_['bulls']:
+                bot_num[i] = str(random.randint(1, 9))
     bot_num = int(''.join(bot_num))
     while not check(human_num, bot_num):
         bot_num = [i for i in str(human_num)]
@@ -72,16 +77,22 @@ while bot_num != human_num:
             if i not in dict_['bulls']:
                 bot_num[i] = str(random.randint(0, 9))
         bot_num = int(''.join(bot_num))
-    human_num = int(input())
 
+    human_num = input()
     while not check_num(human_num):
         print('Некорректный ввод')
-        human_num = int(input())
+        human_num = input()
+    human_num = int(human_num)
 
     bulls_cows = bulls_and_cows(number_to_list(human_num), number_to_list(bot_num))
     print(f'Количество быков: {bulls_cows[0]}')
     print(f'Количество коров: {bulls_cows[1]}')
-
+    for i in dict_['cows']:
+        if str(i) in str(human_num) and str(human_num).index(str(i)) == str(bot_num).index(str(i)):
+            dict_['bulls'].append(i)
+            del dict_['cows'][dict_['cows'].index(i)]
+            
 print('Поздравляю с победой!')
+
 
 
