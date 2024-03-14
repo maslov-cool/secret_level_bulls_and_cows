5c9a93fdca955447ac7b505cd22f3f9c585cb51a
alex <106116143+maslov-cool@users.noreply.github.com>
2024-03-15 01:00:45 +0300

    Update Коровы и быки. Секретный уровень.py
    Окончательный вариант

--- Коровы и быки. Секретный уровень.py
+++ Коровы и быки. Секретный уровень.py
@@ -23,8 +23,8 @@ def bulls_and_cows(list_human_num: list, list_bot_num: list) -> (int, int):
     l2 = list_bot_num
     for k in range(len(l2)):
         if l2[k] == l1[k]:
+            bulls += 1
             if k not in dict_['bulls'].keys():
-                bulls += 1
                 dict_['bulls'][k] = l1[k]
                 if l2[k] in dict_['cows']:
                     del dict_['cows'][dict_['cows'].index(l1[k])]
@@ -45,13 +45,13 @@ def bulls_and_cows(list_human_num: list, list_bot_num: list) -> (int, int):
 
 def check(bot_num_: int) -> bool:
     global dict_
-    l = number_to_list(bot_num_)
-    l = [l[q] for q in range(len(l)) if q not in dict_['bulls'].keys()]
+    l1 = number_to_list(bot_num_)
+    l1 = [l1[q] for q in range(len(l1)) if q not in dict_['bulls'].keys()]
     # в процессе генерации мы генерируем только цифры, индексы которых не в dict_['bulls'],
     # поэтому проверка только на коров
     for i in dict_['cows']:
-        if i in l:
-            del l[l.index(i)]
+        if i in l1:
+            del l1[l1.index(i)]
         else:
             return False
     return True
@@ -74,17 +74,19 @@ print(f'Количество быков: {bulls_cows[0]}')
 print(f'Количество коров: {bulls_cows[1]}')
 
 while bot_num != human_num:
-    bot_num = [i for i in '0000']
-    for i in dict_['bulls'].keys():
-        bot_num[i] = str(dict_['bulls'][i])
+    bot_num = [i for i in str(bot_num)]
+    for i in range(len(bot_num)):
+        if i not in dict_['bulls'].keys():
+            bot_num[i] = random.choice([str(j) for j in range(10) if str(j) != bot_num[i]])
     bot_num = int(''.join(bot_num))
 
     while not check(bot_num):
         bot_num = [i for i in str(bot_num)]
         for i in range(len(bot_num)):
             if i not in dict_['bulls'].keys():
-                bot_num[i] = str(random.randint(0, 9))
+                bot_num[i] = random.choice([str(j) for j in range(10) if str(j) != bot_num[i]])
         bot_num = int(''.join(bot_num))
+
     human_num = input()
     while not check_num(human_num):
         print('Некорректный ввод')
@@ -96,6 +98,7 @@ while bot_num != human_num:
     print(f'Количество коров: {bulls_cows[1]}')
             
 print('Поздравляю с победой!')
+
 
 
 
